3f1f92d02ef7b57fc3fc8c11f816a370e2dc3f24
alexHooper <alexmohack@gmail.com>
2018-09-10 20:51:06 +0530

    use microsoft edge driver with selenium

---
+++ using_selenium.py
@@ -0,0 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver(r"C:\Users\pranav\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\TempState\Downloads\MicrosoftWebDriver (1).exe")
