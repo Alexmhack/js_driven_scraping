d797eb257c30943002277f9307a3b694f65b43e4
alexHooper <alexmohack@gmail.com>
2018-09-10 20:54:37 +0530

    open edge browser with selenium

--- using_selenium.py
+++ using_selenium.py
@@ -2,4 +2,4 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver(r"C:\Users\pranav\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\TempState\Downloads\MicrosoftWebDriver (1).exe")
+driver = webdriver.Edge(r"C:\Users\pranav\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\TempState\Downloads\MicrosoftWebDriver (1).exe")
