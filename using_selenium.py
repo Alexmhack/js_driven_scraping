cb7979fa03b663cdb099fc0a694a0e90fc738adb
alexHooper <alexmohack@gmail.com>
2018-09-11 16:28:38 +0530

    open google using driver

--- using_selenium.py
+++ using_selenium.py
@@ -3,3 +3,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Firefox()
+driver.get('https://www.google.com')
+
+html = driver.execute_script("return document.documentElement.outerHTML")
+py_soup = BeautifulSoup(html, 'html.parser')
